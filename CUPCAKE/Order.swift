a4ab1385231808b4fa15e6cb9f24564ff052d8a4
elhajjaji <elhajjajiadiba@gmail.com>
2020-05-20 15:47:57 +0000

    Encoding an ObservableObject class

--- CUPCAKE/Order.swift
+++ CUPCAKE/Order.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-class Order: ObservableObject {
+class Order: ObservableObject , Codable{
+    enum CodingKeys: CodingKey {
+        case type, quantity, extrafrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
     static let types = ["Vanilla","Strawberry","Chocolate","Rainbow"]
     
     @Published var type = 0
@@ -50,4 +54,38 @@ class Order: ObservableObject {
         return cost
     }
     
+    init() {
+        
+    }
+    
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+
+        extrafrosting = try container.decode(Bool.self, forKey: .extrafrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+           try container.encode(type, forKey: .type)
+           try container.encode(quantity, forKey: .quantity)
+
+           try container.encode(extrafrosting, forKey: .extrafrosting)
+           try container.encode(addSprinkles, forKey: .addSprinkles)
+
+           try container.encode(name, forKey: .name)
+           try container.encode(streetAddress, forKey: .streetAddress)
+           try container.encode(city, forKey: .city)
+           try container.encode(zip, forKey: .zip)
+    
+    }
 }
